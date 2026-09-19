7477f8c2745ddfe9ed5ae40f10c2efbebcc52ced
Mahbub <mahbubalam12890@gmail.com>
2026-09-19 18:42:20 +0600

    Student class created

---
+++ InsertDatafromWebPageToDatabase/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InsertDatafromWebPageToDatabase
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string RegNo { get; set; }
+        public string Name { get; set; }
+        public int age { get; set; }
+        public string Depertment { get; set; }
+        public string Address { get; set; }
+    }
+}
