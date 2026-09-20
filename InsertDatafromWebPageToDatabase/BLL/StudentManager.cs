77dddb7c730d223f3506c53a65e0c95ecdbeb138
Mahbub <mahbubalam12890@gmail.com>
2026-09-20 18:06:33 +0600

    Project Redesign with Layer Architecture

---
+++ InsertDatafromWebPageToDatabase/BLL/StudentManager.cs
@@ -0,0 +1,35 @@
+﻿using InsertDatafromWebPageToDatabase.DAL.Gateway;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace InsertDatafromWebPageToDatabase.BLL
+{
+    public class StudentManager
+    {
+        StudentGateway studentGateway = new StudentGateway();
+        public string SaveStudent (Student student)
+        {
+            
+            int rowAffect = studentGateway.SaveStudent(student);
+            if (rowAffect > 0)
+            {
+                return "Save Successfully";
+            }
+            else
+            {
+                return "Failed to Save ";
+            }
+        }
+
+        public DataTable GetAllStudents()
+        {
+            return studentGateway.GetAllStudents();
+        
+        }
+    }
+
+}
